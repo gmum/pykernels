654f4f870e03958040bcb31fbd53a6fb33b5ed89
Kasia Janocha <kasiajanocha@gmail.com>
2015-08-27 18:44:31 +0200

    RandomWalk dense -> sparse

--- pykernels/graph/randomwalk.py
+++ pykernels/graph/randomwalk.py
@@ -6,6 +6,8 @@ __author__ = 'kasiajanocha'
 
 import numpy as np
 from pykernels.base import Kernel, GraphKernel
+from scipy.sparse import lil_matrix, kron,identity
+from scipy.sparse.linalg import lsqr
 
 def _norm(adj_mat):
     """Normalize adjacency matrix"""
@@ -27,19 +29,22 @@ class RandomWalk(GraphKernel):
     # either tensor of dimention 3 (list of adjacency matrices)
     def _compute(self, data_1, data_2):
         res = np.zeros((len(data_1), len(data_2)))
+        N = len(data_1) * len(data_2)
         for i, graph1 in enumerate(data_1):
             for j, graph2 in enumerate(data_2):
                 # norm1, norm2 - normalized adjacency matrixes
                 norm1 = _norm(graph1)
                 norm2 = _norm(graph2)
                 # if graph is unweighted, W_prod = kron(a_norm(g1)*a_norm(g2))
-                w_prod = np.kron(norm1, norm2)
+                w_prod = kron(lil_matrix(norm1), lil_matrix(norm2))
                 starting_prob = np.ones(w_prod.shape[0]) / (w_prod.shape[0])
                 stop_prob = starting_prob
                 # first solve (I - lambda * W_prod) * x = p_prod
-                A = np.identity(w_prod.shape[0]) - (w_prod * self._lmb)
-                x = np.linalg.lstsq(A, starting_prob)
+                A = identity(w_prod.shape[0]) - (w_prod * self._lmb)
+                x = lsqr(A, starting_prob)
                 res[i, j] = stop_prob.T.dot(x[0])
+                # print float(len(data_2)*i + j)/float(N), "%"
+            print float(i)/float(len(data_1))*100, "%"
         return res
 
     def dim(self):
