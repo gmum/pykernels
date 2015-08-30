4d71cd75b24fc2c1be64e8f27c39a0640d4b9327
lejlot <lejlot@gmail.com>
2015-08-30 15:23:08 +0200

    fixed kernel composition operations

--- pykernels/base.py
+++ pykernels/base.py
@@ -47,24 +47,27 @@ class Kernel(object):
     def __add__(self, kernel):
         return KernelSum(self, kernel)
 
-    def __sub__(self, kernel):
-        return KernelSum(self, -kernel)
+    def __mul__(self, value):
+        if isinstance(value, Kernel):
+            return KernelProduct(self, value)
+        else:
+            if isinstance(self, ScaledKernel):
+                return ScaledKernel(self._kernel, self._scale * value)
+            else:
+                return ScaledKernel(self, value)
 
-    def __mul__(self, scale):
-        return ScaledKernel(self, scale)
-
-    def __rmul__(self, scale):
-        return ScaledKernel(self, scale)
+    def __rmul__(self, value):
+        return self.__mul__(value)
 
     def __div__(self, scale):
         return ScaledKernel(self, 1./scale)
 
-    def __neg__(self):
-        return ScaledKernel(self, -1.)
+    def __pow__(self, value):
+        return KernelPower(self, value)
 
 class KernelSum(Kernel):
     """
-    Represents sum of a set of kernels
+    Represents sum of a pair of kernels
     """
 
     def __init__(self, kernel_1, kernel_2):
@@ -76,8 +79,55 @@ class KernelSum(Kernel):
                self._kernel_2._compute(data_1, data_2)
 
     def dim(self):
-        return self._kernel_1.dim() + \
-               self._kernel_2.dim()
+        # It is too complex to analyze combined dimensionality, so we give a lower bound
+        return max(self._kernel_1.dim(), self._kernel_2.dim())
+
+    def __str__(self):
+        return '(' + str(self._kernel_1) + ' + ' + str(self._kernel_2) + ')'
+
+
+class KernelProduct(Kernel):
+    """
+    Represents product of a pair of kernels
+    """
+
+    def __init__(self, kernel_1, kernel_2):
+        self._kernel_1 = kernel_1
+        self._kernel_2 = kernel_2
+
+    def _compute(self, data_1, data_2):
+        return self._kernel_1._compute(data_1, data_2) * \
+               self._kernel_2._compute(data_1, data_2)
+
+    def dim(self):
+        # It is too complex to analyze combined dimensionality, so we give a lower bound
+        return max(self._kernel_1.dim(), self._kernel_2.dim())
+
+    def __str__(self):
+        return '(' + str(self._kernel_1) + ' * ' + str(self._kernel_2) + ')'
+
+
+class KernelPower(Kernel):
+    """
+    Represents natural power of a kernel
+    """
+
+    def __init__(self, kernel, d):
+        self._kernel = kernel
+        self._d = d
+        if not isinstance(d, int) or d<0:
+            raise Exception('Kernel power is only defined for non-negative integer degrees')
+
+    def _compute(self, data_1, data_2):
+        return self._kernel._compute(data_1, data_2) ** self._d
+
+    def dim(self):
+        # It is too complex to analyze combined dimensionality, so we give a lower bound
+        return self._kernel.dim()
+
+    def __str__(self):
+        return str(self._kernel) + '^' + str(self._d)
+
 
 class ScaledKernel(Kernel):
     """
@@ -87,12 +137,20 @@ class ScaledKernel(Kernel):
     def __init__(self, kernel, scale):
         self._kernel = kernel
         self._scale = scale
+        if scale < 0:
+            raise Exception('Negation of the kernel is not a kernel!')
 
     def _compute(self, data_1, data_2):
         return self._scale * self._kernel._compute(data_1, data_2)
 
     def dim(self):
         return self._kernel.dim()
+
+    def __str__(self):
+        if self._scale == 1.0:
+            return str(self._kernel)      
+        else:
+            return str(self._scale) + ' ' + str(self._kernel)
 
 
 class GraphKernel(Kernel):
